abae8d320b629ea7ede77b4d4cbd0fb957b10b5f
dsaf2007 <dsaf2007@naver.com>
2016-09-21 15:20:25 +0900

    draw red square

---
+++ GLFW/GLFW/소스.cpp
@@ -0,0 +1,116 @@
+#include <GLFW/glfw3.h>
+#include <algorithm>
+
+const int width = 500;
+
+const int height =  500;
+
+
+
+float* pixels = new float[width*height * 3];
+
+
+
+void drawPixel(const int& i, const int& j, const float& red, const float& green, const float& blue)
+
+{
+
+	pixels[(i + width* j) * 3 + 0] = red;
+
+	pixels[(i + width* j) * 3 + 1] = green;
+
+	pixels[(i + width* j) * 3 + 2] = blue;
+
+}
+
+
+
+// scratched from https://courses.engr.illinois.edu/ece390/archive/archive-f2000/mp/mp4/anti.html
+
+// see 'Rasterization' part.
+
+void drawLine(const int& i0, const int& j0, const int& i1, const int& j1, const float& red, const float& green, const float& blue)
+
+{
+	if (i1 == i0)
+	{
+		for (int j = j0; j <= j1; j++)
+		{
+			const int i = i1;
+
+
+			drawPixel(i, j, red, green, blue);
+		}
+	}
+	if (j1 == j0)
+	{
+		for (int i = i0; i <= i1; i++)
+		{
+			const int j = j1;
+
+
+			drawPixel(i, j, red, green, blue);
+		}
+	}if(i0!=i1&&j0!=j1)
+	{
+		for (int i = i0; i <= i1; i++)
+
+		{
+
+			const int j = (j1 - j0)*(i - i0) / (i1 - i0) + j0;
+
+
+			drawPixel(i, j, red, green, blue);
+		}
+
+	}
+
+}
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(width, height, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+		std::fill_n(pixels, width*height * 3, 1.0f);
+
+		drawLine(100, 100, 100, 200, 1.0f, 0.0f, 0.0f);
+		drawLine(100, 200, 199, 200, 1.0f, 0.0f, 0.0f);
+		drawLine(101, 100, 199, 100, 1.0f, 0.0f, 0.0f);
+
+		drawLine(200, 100, 200, 199, 1.0f, 0.0f, 0.0f);
+
+
+
+		glDrawPixels(width, height, GL_RGB, GL_FLOAT, pixels);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	delete[] pixels;
+	glfwTerminate();
+	return 0;
+}
